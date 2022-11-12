ef8ec2ad1a34ace66fa29ecb3df61460761c069f
subramani-barathan <107133945+subramani-barathan@users.noreply.github.com>
2022-11-12 17:19:04 +0000

    product details added

--- src/dummyData.js
+++ src/dummyData.js
@@ -44,3 +44,151 @@ export const userData = [
     },
   ];
  
+  export const productDetails = [
+    {
+      prodID:1,
+      name: 'Peanut Butter Crunchy',
+      image: "4000",
+      incrd:"Roasted Peanuts(90%), Sugar, Hydrogenated Vegetable Oils (Soya Bean or Palm), Salt & Emulsifier (E471).",
+      details: "Naturally delicious real peanut butter power packed with crunchy peanuts and creamy texture. Full of taste and nutrition rich pack not only satisfies your hunger but makes a perfect start of your day.",
+      availPkg: "250 GRM, 283 GRM, 340 GRM,400 GRM, 410 GRM, 500 GRM , 510 GRM, 795 GRM, 1 KG & Bulk Packing As Per Customer Requirement.",
+      pkgNorms: "Cartons & Trays In Shrink Wrapped.",
+      bstBfr: "24 Months",
+      flavour: "Peanut Butter Crunchy",
+      form: "Paste"
+    },
+    {
+      prodID:2,
+      name: 'Peanut Butter Smooth',
+      image: "4000",
+      incrd:"Roasted Peanuts (90%), Sugar, Hydrogenated Vegetable Oils (Soya Bean or Palm), Salt & Emulsifier (E471).",
+      details: "100% smooth and creamy peanut butter prepared with fresh roasted peanuts to spread or to add in cakes/bakes and as a cooking ingredient. The taste you always love with amazing health benefits.",
+      availPkg: "250 GRM, 283 GRM, 340 GRM,400 GRM, 410 GRM, 500 GRM , 510 GRM, 795 GRM, 1 KG & Bulk Packing As Per Customer Requirement.",
+      pkgNorms: "Cartons & Trays In Shrink Wrapped.",
+      bstBfr: "24 Months",
+      flavour: "Peanut Butter Smooth",
+      form: "Paste"
+    },
+    {
+      prodID:3,
+      name: 'Peanut Butter Natural',
+      image: "4000",
+      incrd:"Roasted Peanuts(100%).",
+      details: "",
+      availPkg: "250 GRM, 283 GRM, 340 GRM,400 GRM, 410 GRM, 500 GRM , 510 GRM, 795 GRM, 1 KG & Bulk Packing As Per Customer Requirement.",
+      pkgNorms: "Cartons & Trays In Shrink Wrapped.",
+      bstBfr: "24 Months",
+      flavour: "Peanut Butter Natural",
+      form: "Paste"
+    },
+    {
+      prodID:4,
+      name: 'Peanut Butter Chocolate',
+      image: "4000",
+      incrd:"Roasted Peanuts(80%), Sugar, Hydrogenated Vegetable Oils (Soya Bean or Palm), Dark Chocolate, Cocoa Powder & Salt.",
+      details: "Best peanut butter for chocolate lovers. Enjoy the fantasy of dark chocolate and protein-rich peanut butter to spread and eat daily.",
+      availPkg: "250 GRM, 283 GRM, 340 GRM,400 GRM, 410 GRM, 500 GRM , 510 GRM, 795 GRM, 1 KG & Bulk Packing As Per Customer Requirement.",
+      pkgNorms: "Cartons & Trays In Shrink Wrapped.",
+      bstBfr: "24 Months",
+      flavour: "Peanut Butter Chocolate",
+      form: "Paste"
+    },
+    {
+      prodID:5,
+      name: 'Peanut Butter Honey',
+      image: "4000",
+      incrd:"Roasted Peanuts(90%), Sugar,Honey, Hydrogenated Vegetable Oils (Soya Bean or Palm), Salt & Emulsifier (E471).",
+      details: "Super healthy and tasty honey peanut butter spread. Get double benefits of pure honey and peanuts in ready to eat pack makes you recharge with every bite.",
+      availPkg: "250 GRM, 283 GRM, 340 GRM,400 GRM, 410 GRM, 500 GRM , 510 GRM, 795 GRM, 1 KG & Bulk Packing As Per Customer Requirement.",
+      pkgNorms: "Cartons & Trays In Shrink Wrapped.",
+      bstBfr: "24 Months",
+      flavour: "Peanut Butter Honey",
+      form: "Paste"
+    },
+    {
+      prodID:6,
+      name: 'Peanut Butter Less Fat',
+      image: "4000",
+      incrd:"Roasted Peanuts(67%), Glucose, Maltodextrin, Salt & Emulsifier (E471).",
+      details: "Delicious peanut butter with reduced fat is a good option for those who are calorie conscious. Cut calories and still enjoy the taste of peanut butter.",
+      availPkg: "250 GRM, 283 GRM, 340 GRM,400 GRM, 410 GRM, 500 GRM , 510 GRM, 795 GRM, 1 KG & Bulk Packing As Per Customer Requirement.",
+      pkgNorms: "Cartons & Trays In Shrink Wrapped.",
+      bstBfr: "24 Months",
+      flavour: "Peanut Butter Less Fat",
+      form: "Paste"
+    },
+    {
+      prodID:7,
+      name: 'Peanut Butter Whole Nut',
+      image: "4000",
+      incrd:"Peanuts (94%), Peanut Oil, Emulsifier (E471) & Salt.",
+      details: "Natural nutty goodness of whole peanut butter is a daily health food for active lives. 100% delicious and nutritious whole nut peanut butter spread for regular intake.",
+      availPkg: "250 GRM, 283 GRM, 340 GRM,400 GRM, 410 GRM, 500 GRM , 510 GRM, 795 GRM, 1 KG & Bulk Packing As Per Customer Requirement.",
+      pkgNorms: "Cartons & Trays In Shrink Wrapped.",
+      bstBfr: "24 Months",
+      flavour: "Peanut Butter Whole Nut",
+      form: "Paste"
+    },
+    {
+      prodID:8,
+      name: 'Peanut Butter Sugar Free',
+      image: "4000",
+      incrd:"Roasted Peanuts(90%), Hydrogenated Vegetable Oils (Soya Bean or Palm),Salt & Emulsifier (E471).",
+      details: "Our sugar-free peanut butter is a good source of protein and can be the best option when you want less intake of sugar in your daily diet. Get the world’s most popular spreads at Mother Nutri Foods.",
+      availPkg: "250 GRM, 283 GRM, 340 GRM,400 GRM, 410 GRM, 500 GRM , 510 GRM, 795 GRM, 1 KG & Bulk Packing As Per Customer Requirement.",
+      pkgNorms: "Cartons & Trays In Shrink Wrapped.",
+      bstBfr: "24 Months",
+      flavour: "Peanut Butter Sugar Free",
+      form: "Paste"
+    },
+    {
+      prodID:9,
+      name: 'Roasted Peanuts',
+      image: "4000",
+      incrd:"Peanuts (99%)",
+      details: "",
+      availPkg: "250 GRM, 283 GRM, 340 GRM,400 GRM, 410 GRM, 500 GRM , 510 GRM, 795 GRM, 1 KG & Bulk Packing As Per Customer Requirement.",
+      pkgNorms: "Cartons & Trays In Shrink Wrapped.",
+      bstBfr: "24 Months",
+      flavour: "Roasted Peanut",
+      form: "Natural"
+    },
+    {
+      prodID:10,
+      name: 'Splited Peanuts',
+      image: "4000",
+      incrd:"Peanuts (99%)",
+      details: "",
+      availPkg: "250 GRM, 283 GRM, 340 GRM,400 GRM, 410 GRM, 500 GRM , 510 GRM, 795 GRM, 1 KG & Bulk Packing As Per Customer Requirement.",
+      pkgNorms: "Cartons & Trays In Shrink Wrapped.",
+      bstBfr: "24 Months",
+      flavour: "Splitted Peanut",
+      form: "Natural"
+    },
+    {
+      prodID:11,
+      name: 'Peanut Paste',
+      image: "4000",
+      incrd:"Peanuts (94%), Peanut Oil, Emulsifier (E471) & Salt.",
+      details: "",
+      availPkg: "250 GRM, 283 GRM, 340 GRM,400 GRM, 410 GRM, 500 GRM , 510 GRM, 795 GRM, 1 KG & Bulk Packing As Per Customer Requirement.",
+      pkgNorms: "Cartons & Trays In Shrink Wrapped.",
+      bstBfr: "24 Months",
+      flavour: "Peanut Paste",
+      form: "Paste"
+    },
+    {
+      prodID:12,
+      name: 'Peanut Butter',
+      image: "4000",
+      incrd:"Peanuts (94%), Peanut Oil, Buttor & Salt.",
+      details: "",
+      availPkg: "250 GRM, 283 GRM, 340 GRM,400 GRM, 410 GRM, 500 GRM , 510 GRM, 795 GRM, 1 KG & Bulk Packing As Per Customer Requirement.",
+      pkgNorms: "Cartons & Trays In Shrink Wrapped.",
+      bstBfr: "24 Months",
+      flavour: "Peanut Butter",
+      form: "Paste"
+    }
+  ];
+ 
+  
